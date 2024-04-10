83d913cf19be8082428d0578e5a91b55f02e2a4a
datta-toma <tomadatta15@gmail.com>
2024-04-10 20:59:00 +0600

    banner slider  page careate

---
+++ src/Pages/Banner/Banner.jsx
@@ -0,0 +1,42 @@
+import slide from "../../assets/slide1.jpg"
+import slide1 from "../../assets/slide2.jpg"
+import slide2 from "../../assets/slide3.jpg"
+import slide3 from "../../assets/slide4.jpg"
+
+
+const Banner = () => {
+    return (
+        <div className="carousel w-full ">
+        <div id="slide1" className="carousel-item relative w-full">
+          <img src={slide} className="w-full md:h-3/4 rounded-lg" />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide4" className="btn btn-circle">❮</a> 
+            <a href="#slide2" className="btn btn-circle">❯</a>
+          </div>
+        </div> 
+        <div id="slide2" className="carousel-item relative w-full">
+          <img src={slide1} className="w-full md:h-3/4 rounded-lg" />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide1" className="btn btn-circle">❮</a> 
+            <a href="#slide3" className="btn btn-circle">❯</a>
+          </div>
+        </div> 
+        <div id="slide3" className="carousel-item relative w-full">
+          <img src={slide2} className="w-full md:h-3/4 rounded-lg" />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide2" className="btn btn-circle">❮</a> 
+            <a href="#slide4" className="btn btn-circle">❯</a>
+          </div>
+        </div> 
+        <div id="slide4" className="carousel-item relative w-full">
+          <img src={slide3} className="w-full md:h-3/4 rounded-lg" />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide3" className="btn btn-circle">❮</a> 
+            <a href="#slide1" className="btn btn-circle">❯</a>
+          </div>
+        </div>
+      </div>
+    );
+};
+
+export default Banner;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,7 +1,9 @@
+import Banner from "../Banner/Banner";
 
 const Home = () => {
     return (
         <div>
+            <Banner></Banner>
             <h2>hello home page</h2>
         </div>
     );
